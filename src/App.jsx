b3b085165539bc34787267eca175fdadafb0d3d8
Deividas Stančikas <stancikasdeividas@gmail.com>
2019-05-07 21:06:46 +0300

    Api req
    Pridėti api kreipiniai

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import api from './api';
+import API from './API';
 
 class App extends Component {
   constructor() {
@@ -9,37 +9,38 @@ class App extends Component {
       searchTerm: ''
     };
   }
-
-formSubmitted(event) {
-  event.preventDefault();
-  api.search(this.state.searchTerm)
-    .then(images => {
-      console.log(images);
-      
-    })
-}
+  
+  formSubmitted(event) {
+    event.preventDefault();
+    API.search(this.state.searchTerm)
+      .then(images => {
+        console.log(images);
+      });
+  }
 
   searchTermChanged(event) {
-    console.log(event.target.value);
     this.setState({
       searchTerm: event.target.value
     });
   }
-  render(){
-    const{title, searchTerm} = this.state;
-  return (
-    <div>
-      <h1>{title}</h1>
-      <form onSubmit={(event) => this.formSubmitted(event)}>
-        <label htmlFor="searchTerm">Search Term</label>
-        <input onChange={(event)=>this.searchTermChanged(event)} 
-               value={searchTerm} 
-               className="u-full-width" 
-               type="text" 
-               id="searchTerm" 
-               name="searchTerm"/>
-        <button type="submit">Search</button>
-      </form>
+
+  render() {
+    const { title, searchTerm } = this.state;
+
+    return (
+      <div>
+        <h1>{title}</h1>
+          <form onSubmit={(event) => this.formSubmitted(event)}>
+            <label htmlFor="searchTerm">Search Term</label>
+            <input
+              onChange={(event) => this.searchTermChanged(event)}
+              value={searchTerm}
+              className="u-full-width"
+              type="text"
+              id="searchTerm"
+              name="searchTerm" />
+            <button type="submit">Search</button>
+          </form>
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,12 +1,14 @@
-const API_URL = 'https://api.500px.com/v1/photos/search?'
-export default {
-    search(searchTerm) {
-        const url = `${API_URL}term=${searchTerm}`;
-        return fetch(url)
-            .then(response => response.json())
-            .then(result => {
-                return result.photos;
-            });
+const API_URL = 'https://api.unsplash.com/search/photos?query='
+const clientID ='d0c006a3a8768f252ec217f09ae6f487bf197cb1c7cc2f251b1472c45a24af91'
 
-    }
+export default {
+  search(searchTerm) {
+    const url = `${API_URL}${searchTerm}&client_id=${clientID}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        return result.results;
+      });
+  }
 }
